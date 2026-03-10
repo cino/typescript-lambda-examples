7b67fa83e35155a20726b699bb0198ae0368f0f3
Ricardo Cino <ricardo@cino.io>
2026-03-10 20:36:31 +0100

    chore: dependencies

--- src/api-gateway-http-proxy.ts
+++ src/api-gateway-http-proxy.ts
@@ -6,7 +6,6 @@ import { logger } from './common/powertools';
  * Example Typescript lambda to use while using a REST API Gateway
  * and lambda's to build a powerful api.
  */
-
 export const handler: APIGatewayProxyHandler = async (event, context): Promise<APIGatewayProxyResult> => {
   logger.info(JSON.stringify(event));
   logger.info(JSON.stringify(context));
--- src/api-gateway-rest-proxy.ts
+++ src/api-gateway-rest-proxy.ts
@@ -6,7 +6,6 @@ import { logger } from './common/powertools';
  * Example Typescript lambda to use while using an HTTP API Gateway
  * and lambda's to build a powerful api.
  */
-
 export const handler: APIGatewayProxyHandlerV2 = async (event, context): Promise<APIGatewayProxyResultV2> => {
   logger.info(JSON.stringify(event));
   logger.info(JSON.stringify(context));
